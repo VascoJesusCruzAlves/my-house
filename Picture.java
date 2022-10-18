84d4ef250c3f183d1d6316dd0ccc1a3624779cbc
Vasco <vasco.vjca@gmail.com>
2022-10-18 09:51:11 +0200

    Añadimos cesped y el amanecer

--- Picture.java
+++ Picture.java
@@ -15,7 +15,8 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-
+    private Square grass;
+    
     /**
      * Constructor for objects of class Picture
      */
@@ -51,9 +52,18 @@ public class Picture
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(90);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        
+        grass = new Square();
+        grass.changeColor("green");
+        grass.moveHorizontal(-400);
+        grass.moveVertical(140);
+        grass.changeSize(600);
+        grass.makeVisible();
+        sun.slowMoveVertical(-150);
     }
 
     /**
